c3c30276ada5ed4c2d7fca956d5617a9049019b2
Karan Thakur <thakurkaran0345@gmail.com>
2024-12-29 18:18:42 +0530

    1

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -3,9 +3,10 @@ import type { Config } from "tailwindcss";
 export default {
   darkMode: ["class"],
   content: [
-    "./app/**/*.{ts,tsx}",
-    "../packages/ui/src/**/*.{ts,tsx}", // Include shared components
+	"./app/**/*.{ts,tsx}",
+	"../packages/ui/src/**/*.{ts,tsx}", // Include shared components
   ],
+  
   theme: {
   	extend: {
   		colors: {
--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -3,10 +3,9 @@ import type { Config } from "tailwindcss";
 export default {
     darkMode: ["class"],
     content: [
-      "./src/**/*.{ts,tsx}",
-      "../../apps/web/app/**/*.{ts,tsx}",
-    ],
-
+		"./src/**/*.{ts,tsx}",
+		"../../apps/web/app/**/*.{ts,tsx}",
+	  ],
   theme: {
   	extend: {
   		colors: {
