f7d206cf15faeb496bbc03021a8015e860bfeced
Karan Thakur <thakurkaran0345@gmail.com>
2024-12-30 17:25:30 +0530

    1

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -1,32 +1,29 @@
 import NextAuth from "next-auth";
 import authConfig from "./auth.config";
-import { apiAuthRoute, authRoutes, DEFAULT_LOGIN_REDIRECT, publicRoutes } from "./routes";
+import type { NextRequest } from "next/server";
+import { apiAuthRoute, authRoutes, DEFAULT_LOGIN_REDIRECT, publicRoutes } from "./route";
 
 const { auth } = NextAuth(authConfig);
 
-export default auth((req) => {
+export default auth((req: NextRequest) => {
     const isLoggedIn = !!req.auth;
     const { nextUrl }  = req;
     const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthRoute);
     const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
     const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 
-    if (isApiAuthRoute) {
-        return;
+    if (isApiAuthRoute) return;
+
+    if (isAuthRoute && isLoggedIn) {
+        return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
     }
-    if (isAuthRoute) {
-        if (isLoggedIn) {
-            return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT), nextUrl);
-        }
-        return;
-    }
+
     if (!isLoggedIn && !isPublicRoute) {
-        return Response.redirect(new URL("/auth/login"), nextUrl);
+        return Response.redirect(new URL("/auth/login", nextUrl));
     }
     return;
-})
+});
 
 export const config = {
     matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)']
-
 }
